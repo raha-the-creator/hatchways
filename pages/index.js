9fbe0d082a7caba9e0afd94586734f2236081f50
Ra Du <duisek.kyzylorda@gmail.com>
2023-06-23 12:12:30 -0300

    filter by name & toggle grades

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export async function getServerSideProps() {
   const response = await fetch("https://api.hatchways.io/assessment/students")
@@ -13,6 +13,10 @@ export async function getServerSideProps() {
 }
 
 export default function Home({ students }) {
+  const [query, setQuery] = useState('');
+  const [filteredStudents, setFilteredStudents] = useState(students);
+  const [toggle, setToggle] = useState(false)
+
   const calculateAverageGrade = (grades) => {
     const gradesNumber = grades.map((grade) => Number(grade))
     const sum = gradesNumber.reduce((accum, grade) => accum + grade, 0)
@@ -20,9 +24,36 @@ export default function Home({ students }) {
     return average.toFixed(3)
   }
 
+  const filterStudentsByName = useCallback(() => {
+    let filteredStudents = students
+
+    if(query){
+      filteredStudents = filteredStudents.filter((student) =>
+        student.firstName.toLowerCase().includes(query.toLowerCase())
+      )
+    }
+
+    setFilteredStudents(filteredStudents)
+  }, [students, query])
+
+  useEffect(() => {
+    filterStudentsByName()
+  }, [query, filterStudentsByName])
+
+  function handleToggle(){
+    setToggle(!toggle)
+  }
+
   return (
     <main>
-      {students.map((student) => (
+      <input 
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search by name"
+      />
+
+      {filteredStudents.map((student) => (
         <div className="m-2 border w-3/12" key={student.id}>
           <Image
             src={student.pic}
@@ -34,6 +65,10 @@ export default function Home({ students }) {
           <p>Company: {student.company}</p>
           <p>Skill: {student.skill}</p>
           <p>Average: {calculateAverageGrade(student.grades)}</p>
+          <button onClick={handleToggle}>See grades</button>
+          {toggle && student.grades.map((grade) => (
+            <p>{grade}</p>
+          ))}
         </div>
       ))}
     </main>
